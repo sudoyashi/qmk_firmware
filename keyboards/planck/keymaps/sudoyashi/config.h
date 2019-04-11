07700d4c3206e20fa196ecdd2a304cd982de13f4
sudoyashi <joshuapdhawaii@gmail.com>
2019-04-10 20:39:40 -1000

    modified startup sound

--- keyboards/planck/keymaps/sudoyashi/config.h
+++ keyboards/planck/keymaps/sudoyashi/config.h
@@ -1,7 +1,7 @@
 #pragma once
 #include "song_list.h"
 #ifdef AUDIO_ENABLE
-    #define STARTUP_SONG SONG(PLANCK_SOUND)
+      #define STARTUP_SONG SONG(FF_PRELUDE)
     // #define STARTUP_SONG SONG(NO_SOUND)
 
     #define DEFAULT_LAYER_SONGS { SONG(QWERTY_SOUND), \
