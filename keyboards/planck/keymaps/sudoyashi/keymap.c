4eff030cb04af789477a252d6287d65304d1e954
sudoyashi <joshuapdhawaii@gmail.com>
2019-04-13 09:59:02 -1000

    removed tap dance for UP/DOWN

--- keyboards/planck/keymaps/sudoyashi/keymap.c
+++ keyboards/planck/keymaps/sudoyashi/keymap.c
@@ -63,16 +63,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
       KC_TAB,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN,    KC_QUOT,
       KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT ,
-      KC_LCTL, KC_F4, KC_LGUI,   KC_LALT, LOWER,   KC_SPC,  KC_SPC,  RAISE,   KC_LEFT, TD(TD_DN_PGDN), TD(TD_UP_PGUP), KC_RGHT
+      KC_LCTL, KC_F4, KC_LGUI,   KC_LALT, LOWER,   KC_SPC,  KC_SPC,  RAISE,   KC_LEFT, KC_DOWN, KC_UP, KC_RIGHT
   ),
 
   /* Colemak
    * ,-----------------------------------------------------------------------------------.
    * | Esc  |   Q  |   W  |   F  |   P  |   G  |   J  |   L  |   U  |   Y  |   ;  | Bksp |
    * |------+------+------+------+------+-------------+------+------+------+------+------|
-   * | Tab  |   A  |   R  |   S  |   T  |   D  |   H  |   N  |   E  |   I  |   O  | Enter|
+   * | Tab  |   A  |   R  |   S  |   T  |   D  |   H  |   N  |   E  |   I  |   O  |   '  |
    * |------+------+------+------+------+------|------+------+------+------+------+------|
-   * | Shift|   Z  |   X  |   C  |   V  |   B  |   K  |   M  |   ,  |   .  |   /  | F4   |
+   * | Shift|   Z  |   X  |   C  |   V  |   B  |   K  |   M  |   ,  |   .  |   /  | Enter|
    * |------+------+------+------+------+------+------+------+------+------+------+------|
    * | Ctrl |  F4  | LGUI | Alt  |Lower |    Space    |Raise | Left | Down | Up   |Right |
    * `-----------------------------------------------------------------------------------'
@@ -82,7 +82,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       TD(TD_FULL),  KC_Q,    KC_W,    KC_F,    KC_P,    KC_G,    KC_J,    KC_L,    KC_U,    KC_Y,    TD(CT_SCLN), KC_BSPC,
       KC_TAB,  KC_A,    KC_R,    KC_S,    KC_T,    KC_D,    KC_H,    KC_N,    KC_E,    KC_I,    KC_O,    KC_QUOT,
       KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_K,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT,
-      KC_LCTL, KC_F4, KC_LGUI, KC_LALT , LOWER,  KC_SPC,  KC_SPC,    RAISE,   KC_LEFT, TD(TD_DN_PGDN), TD(TD_UP_PGUP), KC_RIGHT
+      KC_LCTL, KC_F4, KC_LGUI, KC_LALT , LOWER,  KC_SPC,  KC_SPC,    RAISE,   KC_LEFT, KC_DOWN, KC_UP, KC_RIGHT
   ),
 
 
@@ -100,10 +100,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 [_LOWER] = LAYOUT_planck_grid(
     KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_MINUS, KC_EQUAL, KC_BSPC,
-    KC_DEL,  KC_VOLD, KC_VOLU, KC_MPRV, KC_MPLY, KC_MNXT, _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT,  KC_PIPE,
-    KC_LSHIFT, _______,KC_LCBR,KC_LBRC, KC_LABK, KC_LPRN, KC_RPRN, KC_RBRC, KC_LCBR, _______, KC_HOME,   _______,
-    KC_LCTL, KC_F4,   KC_LGUI, KC_LALT, _______, _______, _______, _______, _______, _______, KC_END,    _______
+    KC_DEL,  KC_VOLD, KC_VOLU, KC_MPRV, KC_MPLY, KC_MNXT, _______, _______, _______, _______, _______,  KC_PIPE,
+    KC_LSHIFT, _______,KC_LCBR,KC_LBRC, KC_LABK, KC_LPRN, KC_RPRN, KC_RABK, KC_RBRC, KC_RCBR, KC_HOME,   KC_PGUP,
+    KC_LCTL, KC_F4,   KC_LGUI, KC_LALT, _______, _______, _______, _______, _______, _______, KC_END,    KC_PGDN
 ),
+
 
 /* Raise
  * ,------------------------------------   KC_F5-----------------------------------------------.
--- keyboards/planck/keymaps/sudoyashi/tap.h
+++ keyboards/planck/keymaps/sudoyashi/tap.h
@@ -7,11 +7,8 @@ typedef struct {
 } tap;
 
   enum yashi_custom_kc {
-    TD_UP_PGUP = 0,
-    TD_DN_PGDN = 1,
-    CT_SE = 2,
-    CT_SCLN = 3,
-    TD_FULL = 4
+    CT_SCLN = 0,
+    TD_FULL = 1
   };
 
   void dance_cln_finished (qk_tap_dance_state_t *state, void *user_data) {
@@ -34,8 +31,6 @@ typedef struct {
 
   // Declare tap dance actions
   qk_tap_dance_action_t tap_dance_actions[] = {
-   [TD_UP_PGUP]  = ACTION_TAP_DANCE_DOUBLE(KC_UP, KC_PGUP),
-   [TD_DN_PGDN]  = ACTION_TAP_DANCE_DOUBLE(KC_DOWN, KC_PGDN),
    [CT_SCLN] = ACTION_TAP_DANCE_FN_ADVANCED (NULL, dance_cln_finished, dance_cln_reset),
    [TD_FULL] = ACTION_TAP_DANCE_DOUBLE(KC_ESC, KC_F11)
   };
