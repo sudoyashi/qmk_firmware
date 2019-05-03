7a68cc6ce4542dcb5dbd9caeffb8de2a18e352a8
sudoyashi <joshuapdhawaii@gmail.com>
2019-05-02 16:18:56 -1000

    adding tap.h to levinson

---
+++ keyboards/keebio/levinson/keymaps/sudoyashi/tap.h
@@ -0,0 +1,36 @@
+//Tap Dance Declarations
+#include QMK_KEYBOARD_H
+
+typedef struct {
+  bool is_press_action;
+  int state;
+} tap;
+
+  enum yashi_custom_kc {
+    CT_SCLN = 0,
+    TD_FULL = 1
+  };
+
+  void dance_cln_finished (qk_tap_dance_state_t *state, void *user_data) {
+    if (state->count == 1) {
+      register_code (KC_SCLN);
+    } else {
+      register_code (KC_RSFT);
+      register_code (KC_SCLN);
+    }
+  }
+
+  void dance_cln_reset (qk_tap_dance_state_t *state, void *user_data) {
+    if (state->count == 1) {
+      unregister_code (KC_SCLN);
+    } else {
+      unregister_code (KC_RSFT);
+      unregister_code (KC_SCLN);
+    }
+  }
+
+  // Declare tap dance actions
+  qk_tap_dance_action_t tap_dance_actions[] = {
+   [CT_SCLN] = ACTION_TAP_DANCE_FN_ADVANCED (NULL, dance_cln_finished, dance_cln_reset),
+   [TD_FULL] = ACTION_TAP_DANCE_DOUBLE(KC_ESC, KC_F11)
+  };
