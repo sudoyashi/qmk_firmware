814eebce36b3a5d6a22f41a4899dff55b8d93c24
sudoyashi <joshuapdhawaii@gmail.com>
2019-05-03 09:13:10 -1000

    moving around nav keys

--- keyboards/keebio/levinson/keymaps/sudoyashi/keymap.c
+++ keyboards/keebio/levinson/keymaps/sudoyashi/keymap.c
@@ -53,7 +53,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |Adjust| Ctrl | Alt  | GUI  |Lower |Space |Space |Raise | Left | Down |  Up  |Right |
  * `-----------------------------------------------------------------------------------'
  */
-[_QWERTY] = LAYOUT_ortho_4x12( 
+[_QWERTY] = LAYOUT_ortho_4x12(
   TD(TD_FULL),  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,        KC_BSPC, \
   KC_ESC,       KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    TD(CT_SCLN), KC_QUOT, \
   KC_LSFT,      KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,     KC_ENT , \
@@ -62,7 +62,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
  /* Lower
   * ,-----------------------------------------------------------------------------------.
-  * |   ~  |   !  |   @  |   #  |   $  |   %  |   ^  |   &  |   *  |   (  |   )  | Bksp |
+  * |   ~  |   !  |   @  |   #  |   $  |   %  |   ^  |   &  |   *  |   -  |   )  | Bksp |
   * |------+------+------+------+------+-------------+------+------+------+------+------|
   * | VolDn|VolUp |Prev  |Play  |Next  |Stop  |      |   _  |   +  |   [  |   ]  |  |   |
   * |------+------+------+------+------+------|------+------+------+------+------+------|
@@ -72,8 +72,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   * `-----------------------------------------------------------------------------------'
   */
 [_LOWER] = LAYOUT_ortho_4x12( \
-  KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_BSPC, \
-  KC_VOLD, KC_VOLU, KC_MPRV, KC_MPLY, KC_MNXT, KC_MSTP, _______, KC_LEFT, KC_DOWN, KC_UP, KC_RIGHT, KC_PIPE, \
+  KC_TILD, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC, \
+  KC_VOLD, KC_VOLU, KC_MPRV, KC_MPLY, KC_MNXT, KC_MSTP, _______, _______, _______, _______, _______, KC_PIPE, \
   KC_LSFT, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
   KC_LCTL, KC_F4,   KC_LALT, _______, _______, _______, _______, _______, KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY \
 ),
@@ -90,10 +90,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------------------------------------------------'
  */
 [_RAISE] = LAYOUT_ortho_4x12( \
-  KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC, \
-  KC_DEL,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_INS,  KC_MINS,  KC_HOME, KC_PGUP, KC_BSLS, \
-  KC_LSFT, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______ ,KC_DEL,  KC_END,  KC_PGDN, _______, \
-  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______ \
+  KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    _______, KC_LEFT, KC_DOWN,  KC_UP,   KC_RIGHT, \
+  KC_DEL,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   _______, KC_INS,  KC_HOME,  KC_PGUP, KC_BSLS, \
+  KC_LSFT, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______ ,KC_DEL,  KC_END,  KC_PGDN,  _______, \
+  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,  _______ \
 ),
 
 /* Adjust (Lower + Raise)
