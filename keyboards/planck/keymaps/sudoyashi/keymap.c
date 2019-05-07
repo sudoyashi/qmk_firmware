e0cb9e30d010d8f05bed2e8e8147630377a10ff8
sudoyashi <joshuapdhawaii@gmail.com>
2019-05-06 20:37:17 -1000

    added planck modifications

--- keyboards/planck/keymaps/sudoyashi/keymap.c
+++ keyboards/planck/keymaps/sudoyashi/keymap.c
@@ -24,7 +24,6 @@
 */
 #include QMK_KEYBOARD_H
 #include "tap.h"
-#include "muse.h"
 
 extern keymap_config_t keymap_config;
 
@@ -37,8 +36,8 @@ enum sudoyashi_layers {
 };
 
 enum sudoyashi_keycodes {
-  QWERTY = SAFE_RANGE,
-  COLEMAK,
+  COLEMAK = SAFE_RANGE,
+  QWERTY,
   BACKLIT,
 };
 
@@ -60,9 +59,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    */
   [_QWERTY] = LAYOUT_planck_grid(
       KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
-      KC_TAB,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN,    KC_QUOT,
+      KC_TAB,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
       KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT ,
-      KC_LCTL, KC_F4, KC_LGUI,   KC_LALT, LOWER,   KC_SPC,  KC_SPC,  RAISE,   KC_LEFT, KC_DOWN, KC_UP, KC_RIGHT
+      KC_LCTL, KC_LGUI, KC_LALT, KC_F4 , LOWER,   KC_SPC,  KC_SPC,   RAISE,   KC_LEFT, KC_DOWN, KC_UP, KC_RIGHT
   ),
 
   /* Colemak
@@ -81,7 +80,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       TD(TD_FULL), KC_Q,    KC_W,    KC_F,    KC_P,    KC_G,    KC_J,    KC_L,    KC_U,    KC_Y,    TD(CT_SCLN), KC_BSPC,
       KC_TAB,      KC_A,    KC_R,    KC_S,    KC_T,    KC_D,    KC_H,    KC_N,    KC_E,    KC_I,    KC_O,        KC_QUOT,
       KC_LSFT,     KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_K,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,     KC_ENT,
-      KC_LCTL,     KC_F4,   KC_LGUI, KC_LALT, LOWER,   KC_SPC,  KC_SPC,  RAISE,   KC_LEFT, KC_DOWN, KC_UP,       KC_RIGHT
+      KC_LCTL,     KC_LGUI, KC_LALT, KC_F4 ,  LOWER,   KC_SPC,  KC_SPC,  RAISE,   KC_LEFT, KC_DOWN, KC_UP,       KC_RIGHT
   ),
 
 
@@ -99,9 +98,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 [_LOWER] = LAYOUT_planck_grid(
     KC_TILD,   KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_MINUS, KC_EQUAL, KC_BSPC,
-    KC_DEL,    KC_VOLD, KC_VOLU, KC_MPRV, KC_MPLY, KC_MNXT, KC_LPRN, KC_RPRN, KC_LBRC, KC_RBRC, _______,  KC_PIPE,
+    KC_VOLD,   KC_VOLU, KC_MPRV, KC_MPLY, KC_MNXT, KC_MSTP, KC_LPRN, KC_RPRN, KC_LBRC, KC_RBRC, _______,  KC_PIPE,
     KC_LSHIFT, _______, _______, _______, _______, _______, KC_LABK, KC_RABK, KC_LCBR, KC_RCBR, _______,  _______,
-    KC_LCTL,   KC_F4,   KC_LGUI, KC_LALT, _______, _______, _______, _______, _______, _______, _______,  _______
+    _______,   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,  _______
 ),
 
 
@@ -117,10 +116,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------------------------------------------------'
  */
 [_RAISE] = LAYOUT_planck_grid(
-    KC_GRV,   KC_1,  KC_2,    KC_3,    KC_4,    KC_5,    KC_6,   KC_7,    KC_8,     KC_9,    KC_0,    KC_BSPC,
-    KC_DEL,   KC_F1, KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,  KC_BRID, KC_BRIU,  KC_HOME, KC_PGUP, KC_BSLS,
-    KC_LSHIFT,KC_F7, KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12, _______, _______,  KC_END,  KC_PGDN, _______,
-    KC_LCTL,  KC_F4, KC_LGUI, KC_LALT, _______, _______, _______,_______, _______, _______, _______, _______
+    KC_GRV,   KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,     KC_9,    KC_0,    KC_BSPC,
+    KC_DEL,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   _______, _______, _______, _______, _______,
+    KC_LSHIFT,KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_BRID, KC_BRIU,  KC_END,  KC_PGDN, _______,
+    _______,  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
 ),
 
 /* Adjust (Lower + Raise)
@@ -136,10 +135,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 
 [_ADJUST] = LAYOUT_planck_grid(
-    RESET,  _______,  DEBUG,   _______, _______, _______, KC_PSCREEN, _______, _______, _______, _______, KC_DEL ,
-    AU_ON,  MU_TOG,  MU_MOD,  _______, _______, AG_NORM, AG_SWAP, QWERTY,  COLEMAK, _______, _______, _______,
-    AU_OFF, MUV_DE,   MUV_IN,  MU_ON,   MU_OFF,  MI_ON,   MI_OFF,  TERM_ON, TERM_OFF, _______, _______, _______,
-    _______, _______, _______, _______, _______, _______, _______, _______, _______,  _______, _______, _______
+    RESET,  _______,  DEBUG,   _______, _______, _______, KC_PSCREEN, _______, _______,  _______, _______, KC_DEL ,
+    AU_ON,  MU_TOG,  MU_MOD,  _______, _______,  AG_NORM, AG_SWAP,    QWERTY,  COLEMAK,  _______, _______, _______,
+    AU_OFF, MUV_DE,   MUV_IN,  MU_ON,   MU_OFF,  MI_ON,   MI_OFF,     TERM_ON, TERM_OFF, _______, _______, _______,
+    _______,  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
 )
 
 };
@@ -199,22 +198,6 @@ uint8_t last_muse_note = 0;
 uint16_t muse_counter = 0;
 uint8_t muse_offset = 70;
 uint16_t muse_tempo = 50;
-
-void matrix_scan_user(void) {
-  #ifdef AUDIO_ENABLE
-    if (muse_mode) {
-      if (muse_counter == 0) {
-        uint8_t muse_note = muse_offset + SCALE[muse_clock_pulse()];
-        if (muse_note != last_muse_note) {
-          stop_note(compute_freq_for_midi_note(last_muse_note));
-          play_note(compute_freq_for_midi_note(muse_note), 0xF);
-          last_muse_note = muse_note;
-        }
-      }
-      muse_counter = (muse_counter + 1) % muse_tempo;
-    }
-  #endif
-}
 
 bool music_mask_user(uint16_t keycode) {
   switch (keycode) {
