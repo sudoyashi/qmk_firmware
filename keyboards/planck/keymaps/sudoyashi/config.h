4e12d9fbe547b6b65596be1d58ab8356454a2754
sudoyashi <joshuapdhawaii@gmail.com>
2019-05-07 16:56:22 -1000

    fixing sound issues

--- keyboards/planck/keymaps/sudoyashi/config.h
+++ keyboards/planck/keymaps/sudoyashi/config.h
@@ -4,13 +4,12 @@
       #define STARTUP_SONG SONG(SONIC_RING)
     // #define STARTUP_SONG SONG(NO_SOUND)
 
-    #define DEFAULT_LAYER_SONGS { SONG(ZELDA_TREASURE), \
-                                  SONG(SONIC_RING), \
+    #define DEFAULT_LAYER_SONGS {SONG(SONIC_RING), \
                                 }
 #endif
 
 #define MIDI_BASIC
-#define TAPPING_TERM 200
+#define TAPPING_TERM 150
 
 // Most tactile encoders have detents every 4 stages
 #define ENCODER_RESOLUTION 4
--- keyboards/planck/keymaps/sudoyashi/keymap.c
+++ keyboards/planck/keymaps/sudoyashi/keymap.c
@@ -108,7 +108,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [_RAISE] = LAYOUT_planck_grid(
     KC_GRV,   KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,     KC_9,    KC_0,    KC_BSPC,
-    KC_DEL,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_MINUS, KC_EQUAL, KC_HOME, KC_PGUP, _______,
+    KC_DEL,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_MINUS, KC_EQUAL, KC_HOME, KC_PGUP, KC_BSLS,
     KC_LSHIFT,KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_BRID, KC_BRIU,  KC_END,  KC_PGDN, _______,
     _______,  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
 ),
@@ -171,6 +171,7 @@ uint8_t last_muse_note = 0;
 uint16_t muse_counter = 0;
 uint8_t muse_offset = 70;
 uint16_t muse_tempo = 50;
+
 
 bool music_mask_user(uint16_t keycode) {
   switch (keycode) {
