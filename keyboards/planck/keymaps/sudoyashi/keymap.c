cc5e8cd61eee0e2d047203ba0d22f6d5dda7f5e3
sudoyashi <joshuapdhawaii@gmail.com>
2019-05-13 08:21:05 -1000

    making planck and levinson layout similar

--- keyboards/planck/keymaps/sudoyashi/keymap.c
+++ keyboards/planck/keymaps/sudoyashi/keymap.c
@@ -15,9 +15,8 @@
  *
 */
 #include QMK_KEYBOARD_H
-#include <sendstring_colemak.h>
 #include "muse.h"
-
+;
 extern keymap_config_t keymap_config;
 
 enum layers {
@@ -29,8 +28,8 @@ enum layers {
 };
 
 enum keycodes {
-  QWERTY,
   COLEMAK = SAFE_RANGE,
+  QWERTY,
   COPY,
   PASTE,
   M_EMAIL
@@ -59,7 +58,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       TD(ENT_ESC), KC_Q,    KC_W,    KC_F,    KC_P,    KC_G,    KC_J,    KC_L,    KC_U,    KC_Y,    TD(CT_SCLN), KC_BSPC,
       KC_TAB,      KC_A,    KC_R,    KC_S,    KC_T,    KC_D,    KC_H,    KC_N,    KC_E,    KC_I,    KC_O,        KC_QUOT,
       KC_LSFT,     KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_K,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,     KC_ENT,
-      TD(CT_FULL),     KC_LGUI, KC_LALT, KC_F4 ,  LOWER,   KC_SPC,  KC_SPC,  RAISE,   KC_LEFT, KC_DOWN, KC_UP,       KC_RIGHT
+      TD(CT_FULL), KC_LGUI, KC_LALT, KC_F4 ,  LOWER,   KC_SPC,  KC_SPC,  RAISE,   KC_LEFT, KC_DOWN, KC_UP,       KC_RIGHT
   ),
 
   /* Qwerty
@@ -74,7 +73,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * `-----------------------------------------------------------------------------------'
    */
   [_QWERTY] = LAYOUT_planck_grid(
-      TD(ENT_ESC),      KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
+      TD(ENT_ESC), KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
       KC_TAB,      KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
       KC_LSFT,     KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT ,
       TD(CT_FULL), KC_LGUI, KC_LALT, KC_F4 , LOWER,   KC_SPC,  KC_SPC,   RAISE,   KC_LEFT, KC_DOWN, KC_UP, KC_RIGHT
@@ -95,8 +94,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 [_LOWER] = LAYOUT_planck_grid(
     KC_TILD,   KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_MINUS, KC_EQUAL, KC_BSPC,
-    KC_ESC,    KC_VOLU, KC_MPRV, KC_MPLY, KC_MNXT, KC_MSTP, KC_LPRN, KC_RPRN, KC_LBRC, KC_RBRC, _______,  KC_PIPE,
-    KC_LSHIFT, KC_VOLD, M_EMAIL, TD(CPPS), _______, _______, KC_LABK, KC_RABK, KC_LCBR, KC_RCBR, _______,  _______,
+    KC_ESC,    KC_VOLU, KC_MPRV, KC_MPLY, KC_MNXT, KC_MSTP, KC_LPRN, KC_RPRN,  KC_LBRC, KC_RBRC, _______,  KC_BSLS,
+    KC_LSHIFT, KC_VOLD, M_EMAIL, _______, _______, _______, KC_LABK, KC_RABK, KC_LCBR, KC_RCBR, _______,  _______,
     _______,   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,  _______
 ),
 
@@ -113,10 +112,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------------------------------------------------'
  */
 [_RAISE] = LAYOUT_planck_grid(
-    KC_GRV,   KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,     KC_9,    KC_0,    KC_BSPC,
-    KC_DEL,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_MINUS, KC_EQUAL, KC_HOME, KC_PGUP, KC_BSLS,
-    KC_LSHIFT,KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_BRID, KC_BRIU,  KC_END,  KC_PGDN, _______,
-    _______,  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
+    KC_GRV,   KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,     KC_9,   KC_0,    KC_BSPC,
+    KC_DEL,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   _______, KC_HOME, KC_END,  KC_PGUP, KC_PGDN,
+    KC_LSHIFT,KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT,
+    BL_STEP,  BL_OFF,  BL_DEC,  BL_INC,  _______, _______, _______, _______, _______, _______, _______, _______
 ),
 
 /* Adjust (Lower + Raise)
@@ -132,10 +131,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 
 [_ADJUST] = LAYOUT_planck_grid(
-    RESET,   _______, DEBUG,   _______, _______, _______, KC_PSCREEN, _______, _______,  _______, _______, KC_DEL ,
-    AU_ON,   MU_TOG,  MU_MOD,  _______, _______,  AG_NORM,AG_SWAP,   QWERTY,  COLEMAK,  _______, _______, _______,
-    AU_OFF,  MUV_DE,  MUV_IN,  MU_ON,   MU_OFF,  MI_ON,   MI_OFF,    TERM_ON, TERM_OFF, _______, _______, _______,
-    _______, _______, _______, _______, _______, _______, _______,   _______, _______, _______, _______, _______
+    RESET,   _______, DEBUG,   _______, _______, _______, _______, _______, _______, _______, _______, KC_PSCR ,
+    AU_ON,   MU_TOG,  MU_MOD,  _______, _______, AG_SWAP, AG_NORM, _______, _______, _______, _______, QWERTY,
+    AU_OFF,  MUV_DE,  MUV_IN,  MU_ON,   MU_OFF,  MI_ON,   MI_OFF,  _______, _______, _______, _______, COLEMAK,
+    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
 )
 
 };
